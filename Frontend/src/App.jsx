3e6d9db35f00b4b59c023b75a8844abeb34cc95e
Simran Shah <shahsimran453@gmail.com>
2024-12-29 22:40:52 +0530

    completed view more details

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './Pages/Login';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile'
 import Projects from './Pages/Projects'
+import ViewMoreDetails from './Pages/ViewMoreDetails'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentHeader from './Components/StudentsHeader';
 import Footer from './Components/Footer';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Projects" element={<Projects />} />
+        <Route path="/ViewMoreDetails" element={<ViewMoreDetails />} />
       </Routes>
       <Footer/>
     </Router>
