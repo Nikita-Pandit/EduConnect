09fa0dfeccbcd5f730bc5b18f2334f4fa8f6cce4
Nikita Pandit <omotto896@gmail.com>
2025-05-04 16:06:35 +0530

    Update signupController.js

--- Backend/Controllers/signupController.js
+++ Backend/Controllers/signupController.js
@@ -35,6 +35,9 @@ const signupController = async (req, res) => {
             });
             await user.save(); // Save teacher to the database
             console.log("User created:", user);
+             return res.redirect(
+      `https://educonnect-1-jv7g.onrender.com/SignUp?id=${user._id}&role=${role}`
+    );
             
 // console.log("User isVerified status:", user.isVerified);
         } else {
